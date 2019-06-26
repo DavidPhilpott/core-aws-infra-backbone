83fbd42e14343ec19ac69c230c82b07d3eaa940a
David Philpott <david.philpott@sainsburys.co.uk>
2019-06-26 08:34:27 +0100

    Added SNS setup terraform

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,3 +1,44 @@
-resource "aws_sns_topic" "test_sns_topic" {
-  name = "test-topic-for-infra-backbone"
+resource "aws_sns_topic" "backbone_message_inbox" {
+  name = "backbone-message-inbox"
+}
+
+resource "aws_sns_topic_policy" "backbone_message_inbox_topic_policy" {
+  arn    = aws_sns_topic.backbone_message_inbox.arn
+  policy = data.aws_iam_policy_document.backbone_message_inbox_topic_policy_document.json
+}
+
+data "aws_iam_policy_document" "backbone_message_inbox_topic_policy_document" {
+  statement {
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:AddPermission",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        "${var.aws_account_id}",
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      "${aws_sns_topic.backbone_message_inbox.arn}",
+    ]
+
+  }
 }
