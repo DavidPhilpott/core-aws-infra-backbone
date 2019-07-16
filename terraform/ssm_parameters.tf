10971f95816531a257b3cfcd7771c22c71ea7373
David Philpott <david.philpott@sainsburys.co.uk>
2019-07-16 08:45:57 +0100

    Added core sns to parameter store

---
+++ terraform/ssm_parameters.tf
@@ -0,0 +1,13 @@
+resource "aws_ssm_parameter" "core_message_inbox_arn_parameter" {
+  name        = "core-message-inbox-arn"
+  description = "ARN of the core message inbox."
+  type        = "String"
+  value       = core_message_inbox.arn
+  
+  tags = merge(
+    {
+      "Name" = "core-message-inbox-arn"
+    },
+    local.common_tags
+  )
+}
