3f2c36332f168c2074dd24c4438749e52c9bca4d
David Philpott <DavidJohnPhilpott@outlook.com>
2019-09-02 21:26:11 +0100

    Added basic data lake bucket

---
+++ terraform/data-lake-s3.tf
@@ -0,0 +1,12 @@
+//S3 Bucket acting as a data lake for all incoming data
+resource "aws_s3_bucket" "data_lake" {
+  bucket = "core-data-lake"
+  acl    = "private"
+
+  tags = merge(
+    {
+      "Name" = "core-data-lake"
+    },
+    local.common_tags
+  )
+}
