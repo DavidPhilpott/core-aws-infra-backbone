3978970b8414bb84999a193a46418e88afcd0778
David Philpott <DavidJohnPhilpott@outlook.com>
2019-09-02 21:32:37 +0100

    Modified data lake name

--- terraform/data-lake-s3.tf
+++ terraform/data-lake-s3.tf
@@ -1,6 +1,6 @@
 //S3 Bucket acting as a data lake for all incoming data
 resource "aws_s3_bucket" "data_lake" {
-  bucket = "core-data-lake"
+  bucket = "dp-core-data-lake
   acl    = "private"
 
   tags = merge(
